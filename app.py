5c4be55fff1f16d120853f42a161480a4ffb6d6b
Timothy Doolan <tim0git>
2022-04-22 09:26:33 +0100

    feat: add ses client and hard code email into lambda

--- app.py
+++ app.py
@@ -1,22 +1,113 @@
-import logging
 import json
+import logging
+import os
+import boto3
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event):
-    logger.info("Received event")
-    logger.info(json.dumps(event))
+def get_email_template():
+    SUBJECT = "Amazon SES Test (SDK for Python)"
+
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                 )
+
+    BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (SDK for Python)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+      AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+            """
+
+    template = {
+        "SUBJECT": SUBJECT,
+        "BODY_TEXT": BODY_TEXT,
+        "BODY_HTML": BODY_HTML
+    }
+    return template
+
+
+def get_name_from_dynamodb(event):
+    name = event["Records"][0]["dynamodb"]["NewImage"]["Name"]["S"]
+    return name
+
+
+def get_email_from_dynamodb(event):
+    email = event["Records"][0]["dynamodb"]["NewImage"]["Email"]["S"]
+    return email
+
+
+def get_env_vars():
+    return {
+        "CHARSET": os.environ.get("CHARSET"),
+        "SENDER": os.environ.get("SENDER")
+    }
+
+
+def send_email(email, name, charset, sender, subject, body_text, body_html):
+    client = boto3.client('ses', region_name=os.environ.get('AWS_REGION'))
+    logger.info("Sending email to Name: " + name + " " + "at address Email: " + email)
     try:
-        return {
-            "statusCode": 200,
-            'body': json.dumps({'message': 'success'})
-        }
-    except Exception as e:
-        logger.error(str(e))
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    email,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': charset,
+                        'Data': body_html,
+                    },
+                    'Text': {
+                        'Charset': charset,
+                        'Data': body_text,
+                    },
+                },
+                'Subject': {
+                    'Charset': charset,
+                    'Data': subject,
+                },
+            },
+            Source=sender,
+        )
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
         return {
             'statusCode': 500,
             'body': json.dumps({'message': 'error'})
         }
+    else:
+        logger.info("Email sent message ID: {}".format(response['MessageId']))
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'success', "messageId": response['MessageId']})
+        }
 
+
+def lambda_handler(event):
+    logger.info("Received event")
+
+    env_vars = get_env_vars()
+    email = get_email_from_dynamodb(event)
+    name = get_name_from_dynamodb(event)
+    template = get_email_template()
+
+    return send_email(email,
+                      name,
+                      env_vars["CHARSET"],
+                      env_vars["SENDER"],
+                      template["SUBJECT"],
+                      template["BODY_TEXT"],
+                      template["BODY_HTML"]
+                      )
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,5 +1,11 @@
+import json
+import os
+import boto3
+from moto import mock_ses
 import pytest
 import app
+from testfixtures import log_capture
+import logging
 
 
 @pytest.fixture()
@@ -50,7 +56,103 @@ def apigw_event():
     }
 
 
+verified_email = 'sender@example.com'
+unverified_email = 'sender@unverifiedemail.com'
+
+os.environ['SENDER'] = 'Sender Name <{}>'.format(verified_email)
+os.environ['AWS_REGION'] = 'eu-west-1'
+os.environ['CHARSET'] = 'UTF-8'
+
+
+def set_up_email_client(email_address):
+    client = boto3.client("ses", region_name=os.environ['AWS_REGION'])
+
+    client.verify_email_address(
+        EmailAddress=email_address
+    )
+    return client
+
+
 # Success
+@mock_ses
 def test_returns_status_code_200_if_successful(apigw_event):
-    ret = app.lambda_handler(apigw_event)
-    assert ret["statusCode"] == 200
+    set_up_email_client(verified_email)
+    res = app.lambda_handler(apigw_event)
+    assert res['statusCode'] == 200
+
+
+@mock_ses
+def test_returns_message_id_if_successful(apigw_event):
+    set_up_email_client(verified_email)
+    res = app.lambda_handler(apigw_event)
+    body = json.loads(res["body"])
+    assert isinstance(body['messageId'], str)
+
+
+@mock_ses
+def test_ses_attempts_delivery_if_successful(apigw_event):
+    client = set_up_email_client(verified_email)
+    app.lambda_handler(apigw_event)
+    response = client.get_send_statistics()
+    send_data_points = response['SendDataPoints']
+    assert send_data_points[0]["DeliveryAttempts"] == 1
+
+
+# Error
+@mock_ses
+def test_returns_status_code_500_if_unsuccessful(apigw_event):
+    set_up_email_client(unverified_email)
+    res = app.lambda_handler(apigw_event)
+    assert res['statusCode'] == 500
+
+
+@mock_ses
+def test_returns_error_message_if_unsuccessful(apigw_event):
+    set_up_email_client(unverified_email)
+    res = app.lambda_handler(apigw_event)
+    body = json.loads(res["body"])
+    assert body["message"] == "error"
+
+
+@mock_ses
+def test_ses_does_not_attempt_delivery_if_from_email_is_not_verified(apigw_event):
+    client = set_up_email_client(unverified_email)
+    app.lambda_handler(apigw_event)
+    response = client.get_send_statistics()
+    send_data_points = response['SendDataPoints']
+    assert send_data_points[0]["DeliveryAttempts"] == 0
+
+
+# Logging
+credential_log = ('botocore.credentials', 'INFO', 'Found credentials in environment variables.')
+event_received_log = ('root', 'INFO', 'Received event')
+sending_test_email_log = ('root', 'INFO', 'Sending email to Name: test at address Email: test@gmail.com')
+
+
+@mock_ses
+@log_capture(level=logging.INFO)
+def test_logs_info_request_and_success(capture, apigw_event):
+    set_up_email_client(verified_email)
+    res = app.lambda_handler(apigw_event)
+    body = json.loads(res["body"])
+
+    capture.check(
+        credential_log,
+        event_received_log,
+        sending_test_email_log,
+        ('root', 'INFO', 'Email sent message ID: {}'.format(body['messageId'])),
+    )
+
+
+@mock_ses
+@log_capture(level=logging.INFO)
+def test_logs_info_request_and_error_when_sender_email_is_unverified(capture, apigw_event):
+    set_up_email_client(unverified_email)
+    app.lambda_handler(apigw_event)
+
+    capture.check(
+        credential_log,
+        event_received_log,
+        sending_test_email_log,
+        ('root', 'ERROR', 'Email address not verified Sender Name <{}>'.format(verified_email)),
+    )
